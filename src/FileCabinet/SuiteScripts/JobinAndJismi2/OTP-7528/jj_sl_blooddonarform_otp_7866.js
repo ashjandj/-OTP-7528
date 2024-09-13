23e06884e278f9921f4d6f0ee14f59b8a6f7165a
ashjandj <aiswarya.babu@jobinandjismi.com>
2024-09-13 09:35:34 +0530

    otp-7866 -feat: submitting otp-7866

--- src/FileCabinet/SuiteScripts/JobinAndJismi2/OTP-7528/jj_sl_blooddonarform_otp_7866.js
+++ src/FileCabinet/SuiteScripts/JobinAndJismi2/OTP-7528/jj_sl_blooddonarform_otp_7866.js
@@ -1,8 +1,30 @@
 /**
  * @NApiVersion 2.1
  * @NScriptType Suitelet
+ * * ***********************************************************************************************************************************
+ * Client Name: Nil
+ * 
+ * Jira Code: OTP-7866
+ * 
+ * Title: Custom form to store blood donor details and track them in database
+ * 
+ * Author: Jobin And Jismi IT Services LLP
+ * 
+ * Date Created: 13 - 09 - 2024
+ *
+ * Script Description:
+ * This Suitelet script manages blood donation details through a custom form in NetSuite. It collects information like the donor's name, gender, phone number, blood group, and last donation date. Upon submission, it creates a custom record in NetSuite.
+ * - Custom Form: Captures and validates required donor information.
+ * - Record Creation: Submits data to create a custom blood donation record.
+ * - Error Handling: Logs errors and provides user feedback.
+ * 
+
+ * 
+ * Revision History: 1.0
+ *************************************************************************************************************************************8
+
  */
-define(['N/record','N/ui/serverWidget','N/format'],
+define(['N/record', 'N/ui/serverWidget', 'N/format'],
     /**
  * @param{record} record
  */
@@ -17,10 +39,9 @@ define(['N/record','N/ui/serverWidget','N/format'],
         const onRequest = (scriptContext) => {
             if (scriptContext.request.method === 'GET') {
                 let form = createBloodDonationForm();
-                try{
+                try {
                     scriptContext.response.writePage(form);
-                }catch(err)
-                {
+                } catch (err) {
                     log.error("Error creating Form", err);
                     scriptContext.response.write(`<h1 style= "color:red">Something went wrong</h1>`);
                 }
@@ -31,32 +52,39 @@ define(['N/record','N/ui/serverWidget','N/format'],
                     </h3>`);
             }
         }
-
+        /**
+            * Creates a Blood Donation Registration form with fields for donor details such as first name, last name, gender, phone number, blood group, and last donation date.
+            * The form is used to collect and validate the required data for blood donors.
+            *
+            * @returns {serverWidget.Form} The form object representing the Blood Requirement Registration Form.
+            * 
+            * @throws {Error} Logs an error if there is an issue creating the form.
+        */
         function createBloodDonationForm() {
-            try{
+            try {
                 let form = serverWidget.createForm({
                     title: 'Blood Requirement Registration Form'
                 });
-    
+
                 let firstName = form.addField({
                     id: 'custpage_jj_first_name_otp7866',
                     type: serverWidget.FieldType.TEXT,
                     label: 'First Name'
                 });
                 firstName.isMandatory = true;
-    
+
                 let lastName = form.addField({
                     id: 'custpage_jj_last_name_otp7866',
                     type: serverWidget.FieldType.TEXT,
                     label: 'Last Name'
                 });
-                
+
                 let gender = form.addField({
                     id: 'custpage_jj_gender_otp7866',
                     type: serverWidget.FieldType.SELECT,
                     label: 'Gender'
                 });
-                
+
                 gender.addSelectOption({
                     value: 1,
                     text: "Male"
@@ -69,7 +97,7 @@ define(['N/record','N/ui/serverWidget','N/format'],
                     value: 3,
                     text: "Custom"
                 });
-    
+
                 let phoneNumber = form.addField({
                     id: 'custpage_jj_phone_otp7866',
                     type: serverWidget.FieldType.PHONE,
@@ -96,7 +124,7 @@ define(['N/record','N/ui/serverWidget','N/format'],
                 });
                 bloodGroup.addSelectOption({
                     text: "B-",
-                    value:4
+                    value: 4
                 });
                 bloodGroup.addSelectOption({
                     text: "AB+",
@@ -125,66 +153,79 @@ define(['N/record','N/ui/serverWidget','N/format'],
                 });
                 return form;
             }
-            catch(err)
-            {
-                log.error("Error in the function createBloodDonationForm",err);
+            catch (err) {
+                log.error("Error in the function createBloodDonationForm", err);
             }
         }
-        function createBloodDonarRecord(scriptContextParameters)
-        {
-            try{
-            let firstName = scriptContextParameters.custpage_jj_first_name_otp7866;
-            let lastName = scriptContextParameters.custpage_jj_last_name_otp7866;
-            let gender = scriptContextParameters.custpage_jj_gender_otp7866;
-            let phoneNumber = scriptContextParameters.custpage_jj_phone_otp7866;
-            let lastDonationDate = scriptContextParameters.custpage_jj_last_donation_otp7866;
-            let bloodGroup = scriptContextParameters.custpage_jj_blood_group_otp7866;
 
-            let bloodDonationForm = record.create({
-                type: "customrecord_jj_blood_requirment",
-                isDynamic: true,
-            });
+        /**
+            * Creates a new blood donor record based on the form data provided in the `scriptContextParameters`.
+            * The data is used to populate a custom record for blood donation requirements, with fields like first name, last name, gender, phone number, blood group, and last donation date.
+            * 
+            * @param {Object} scriptContextParameters - The parameters containing the form field values submitted by the user.
+            * @param {string} scriptContextParameters.custpage_jj_first_name_otp7866 - The first name of the donor.
+            * @param {string} scriptContextParameters.custpage_jj_last_name_otp7866 - The last name of the donor.
+            * @param {string} scriptContextParameters.custpage_jj_gender_otp7866 - The gender of the donor.
+            * @param {string} scriptContextParameters.custpage_jj_phone_otp7866 - The phone number of the donor.
+            * @param {string} scriptContextParameters.custpage_jj_blood_group_otp7866 - The blood group of the donor.
+            * @param {string} scriptContextParameters.custpage_jj_last_donation_otp7866 - The last donation date of the donor.
+            * 
+            * @returns {number|null} The ID of the created blood donor record, or null if an error occurs.
+            *   
+            * @throws {Error} Logs an error if there is an issue creating or saving the donor record.
+        */
+        function createBloodDonarRecord(scriptContextParameters) {
+            try {
+                let firstName = scriptContextParameters.custpage_jj_first_name_otp7866;
+                let lastName = scriptContextParameters.custpage_jj_last_name_otp7866;
+                let gender = scriptContextParameters.custpage_jj_gender_otp7866;
+                let phoneNumber = scriptContextParameters.custpage_jj_phone_otp7866;
+                let lastDonationDate = scriptContextParameters.custpage_jj_last_donation_otp7866;
+                let bloodGroup = scriptContextParameters.custpage_jj_blood_group_otp7866;
 
-            try{
-                bloodDonationForm.setValue({
-                    fieldId: "custrecord_jj_first_name_otp7866",
-                    value: firstName
+                let bloodDonationForm = record.create({
+                    type: "customrecord_jj_blood_requirment",
+                    isDynamic: true,
                 });
 
-                bloodDonationForm.setValue({
-                    fieldId: "custrecord_jj_last_name_otp7866",
-                    value: lastName
-                });
-                bloodDonationForm.setValue({
-                    fieldId: "custrecord_jj_gender_otp7866",
-                    value: gender
-                });
-                bloodDonationForm.setValue({
-                    fieldId: "custrecord_jj_phone_otp7866",
-                    value: phoneNumber
-                });
-                bloodDonationForm.setValue({
-                    fieldId: "custrecord_jj_blood_group_otp7866",
-                    value: bloodGroup
-                });
-                let loadDate = format.parse({value: lastDonationDate, type: format.Type.DATE});
-                bloodDonationForm.setValue({
-                    fieldId: "custrecord_jj_last_donation_otp7866",
-                    value: loadDate
-                });
-            }catch(err)
-            {
-                log.debug("Issue with the entered value",err);
-            }
+                try {
+                    bloodDonationForm.setValue({
+                        fieldId: "custrecord_jj_first_name_otp7866",
+                        value: firstName
+                    });
 
-            let recordId = bloodDonationForm.save({
-                enableSourcing: true,
-                ignoreMandatoryFields: false
-            });
+                    bloodDonationForm.setValue({
+                        fieldId: "custrecord_jj_last_name_otp7866",
+                        value: lastName
+                    });
+                    bloodDonationForm.setValue({
+                        fieldId: "custrecord_jj_gender_otp7866",
+                        value: gender
+                    });
+                    bloodDonationForm.setValue({
+                        fieldId: "custrecord_jj_phone_otp7866",
+                        value: phoneNumber
+                    });
+                    bloodDonationForm.setValue({
+                        fieldId: "custrecord_jj_blood_group_otp7866",
+                        value: bloodGroup
+                    });
+                    let loadDate = format.parse({ value: lastDonationDate, type: format.Type.DATE });
+                    bloodDonationForm.setValue({
+                        fieldId: "custrecord_jj_last_donation_otp7866",
+                        value: loadDate
+                    });
+                } catch (err) {
+                    log.error("Issue with the entered value", err);
+                }
 
-            return recordId;
-            }catch(err)
-            {
+                let recordId = bloodDonationForm.save({
+                    enableSourcing: true,
+                    ignoreMandatoryFields: false
+                });
+
+                return recordId;
+            } catch (err) {
                 log.error("Error in the function createBloodDonarRecord", err);
             }
 
