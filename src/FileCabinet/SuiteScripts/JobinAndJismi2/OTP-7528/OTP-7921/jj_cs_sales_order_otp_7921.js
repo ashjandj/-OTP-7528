785c51cbf1e6fa2519c3a4e61423cde39694d483
ashjandj <aiswarya.babu@jobinandjismi.com>
2024-09-25 15:32:11 +0530

    otp 7921 -test:submitted for testing

--- src/FileCabinet/SuiteScripts/JobinAndJismi2/OTP-7528/OTP-7921/jj_cs_sales_order_otp_7921.js
+++ src/FileCabinet/SuiteScripts/JobinAndJismi2/OTP-7528/OTP-7921/jj_cs_sales_order_otp_7921.js
@@ -33,21 +33,6 @@ define(['N/record', 'N/search', 'N/format'],
      */
     function (record, search, format) {
 
-
-        /**
-         * Function to be executed after page is initialized.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.mode - The mode in which the record is being accessed (create, copy, or edit)
-         *
-         * @since 2015.2
-         */
-        function pageInit(scriptContext) {
-
-
-        }
-
         /**
          * Function to be executed when field is changed.
          *
@@ -68,121 +53,7 @@ define(['N/record', 'N/search', 'N/format'],
             }
         }
 
-        /**
-         * Function to be executed when field is slaved.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         * @param {string} scriptContext.fieldId - Field name
-         *
-         * @since 2015.2
-         */
-        function postSourcing(scriptContext) {
-
-        }
-
-        /**
-         * Function to be executed after sublist is inserted, removed, or edited.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         *
-         * @since 2015.2
-         */
-        function sublistChanged(scriptContext) {
-
-        }
-
-        /**
-         * Function to be executed after line is selected.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         *
-         * @since 2015.2
-         */
-        function lineInit(scriptContext) {
-
-        }
-
-        /**
-         * Validation function to be executed when field is changed.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         * @param {string} scriptContext.fieldId - Field name
-         * @param {number} scriptContext.lineNum - Line number. Will be undefined if not a sublist or matrix field
-         * @param {number} scriptContext.columnNum - Line number. Will be undefined if not a matrix field
-         *
-         * @returns {boolean} Return true if field is valid
-         *
-         * @since 2015.2
-         */
-        function validateField(scriptContext) {
-
-        }
-
-        /**
-         * Validation function to be executed when sublist line is committed.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         *
-         * @returns {boolean} Return true if sublist line is valid
-         *
-         * @since 2015.2
-         */
-        function validateLine(scriptContext) {
-
-        }
-
-        /**
-         * Validation function to be executed when sublist line is inserted.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         *
-         * @returns {boolean} Return true if sublist line is valid
-         *
-         * @since 2015.2
-         */
-        function validateInsert(scriptContext) {
-
-        }
-
-        /**
-         * Validation function to be executed when record is deleted.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @param {string} scriptContext.sublistId - Sublist name
-         *
-         * @returns {boolean} Return true if sublist line is valid
-         *
-         * @since 2015.2
-         */
-        function validateDelete(scriptContext) {
-
-        }
-
-        /**
-         * Validation function to be executed when record is saved.
-         *
-         * @param {Object} scriptContext
-         * @param {Record} scriptContext.currentRecord - Current form record
-         * @returns {boolean} Return true if record is valid
-         *
-         * @since 2015.2
-         */
-        function saveRecord(scriptContext) {
-
-        }
+        
         /**
             * Updates the sales order sublist on the form based on selected filters.
             *
@@ -441,7 +312,7 @@ define(['N/record', 'N/search', 'N/format'],
             * - Returns an array where each object contains the internal ID and the total gross amount for the corresponding sales order.
         */
         function createsubTotalArrayResult() {
-            var subtotalorderSearchObj = search.create({
+            let subtotalorderSearchObj = search.create({
                 type: search.Type.SALES_ORDER,
                 title: 'JJ sales order Search31',
                 id: 'customsearch_jj_so_search31',
@@ -472,19 +343,18 @@ define(['N/record', 'N/search', 'N/format'],
 
             let subTotalArrayResult = [];
             subtotalorderSearchObj.run().each(function (result) {
-                // Get the values from the result
-                var internalId = result.getValue({
+                let internalId = result.getValue({
                     name: "internalid",
                     summary: "GROUP"
                 });
 
-                var grossAmount = result.getValue({
+                let grossAmount = result.getValue({
                     name: "grossamount",
                     summary: "SUM"
                 });
 
                 subTotalArrayResult.push({ internalId: internalId, grossAmount: grossAmount })
-                return true; // Continue to the next result
+                return true;
             });
             return subTotalArrayResult;
         }
@@ -520,16 +390,8 @@ define(['N/record', 'N/search', 'N/format'],
         }
 
         return {
-            //pageInit: pageInit,
-            fieldChanged: fieldChanged,
-            // postSourcing: postSourcing,
-            // sublistChanged: sublistChanged,
-            // lineInit: lineInit,
-            // validateField: validateField,
-            // validateLine: validateLine,
-            // validateInsert: validateInsert,
-            // validateDelete: validateDelete,
-            // saveRecord: saveRecord
+            fieldChanged: fieldChanged
+           
         };
 
     });
